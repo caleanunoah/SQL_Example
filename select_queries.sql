ceb7d9f02db63688d124f0696f19a2f8c1d4fdc1
caleanunoah <noahcaleanu152@gmail.com>
2021-02-08 20:50:37 -0800

    fixed comments with descriptions of queries

--- select_queries.sql
+++ select_queries.sql
@@ -15,7 +15,7 @@ WHERE gender = "M" OR gender = "F"
 GROUP BY gender 
 ORDER BY gender DESC;
 
--- Query 5: Do men and women get paid equally? (Spoiler alert, they do :D ) --
+-- Query 3: Do men and women get paid equally? (Spoiler alert, they do :D ) --
 SELECT 
 	employees.gender, 
     AVG(salaries.salary) as "Average Salary $"
@@ -23,21 +23,10 @@ FROM employees
 INNER JOIN salaries ON employees.emp_no=salaries.emp_no
 GROUP BY gender;
 
-
-
-
-
-
--- IGNORE BELOW. Validating query 5 by calculating individually--
-/*
-SELECT employees.gender, AVG(salaries.salary)
+-- Query 4: Find all employees hired within the year 2000 --
+SELECT
+	emp_no as "Employee #",
+    hire_date
 FROM employees
-INNER JOIN salaries ON employees.emp_no=salaries.emp_no
-WHERE employees.gender = "F";
-
-SELECT employees.gender, AVG(salaries.salary)
-FROM employees
-INNER JOIN salaries ON employees.emp_no=salaries.emp_no
-WHERE employees.gender = "M";
-*/
-
+WHERE YEAR(hire_date) LIKE "%2000%"
+ORDER BY emp_no;
