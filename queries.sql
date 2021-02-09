f0a52b0017c4bd4ac0d5ee41eab8b6a3f8008615
caleanunoah <noahcaleanu152@gmail.com>
2021-02-08 20:26:13 -0800

    Added query to check if gender pay gap is equal

--- queries.sql
+++ queries.sql
@@ -1,25 +1,43 @@
--- Use github open source database, Employees -- 
+-- Use github open source database, employees -- 
 USE employees;
 
--- Write an SQL query to format full names from employee table --
+-- Query 1: Write an SQL query to format full names their employee number --
 SELECT 
-	CONCAT(first_name, " ", last_name) AS "Full Name"
+	 emp_no AS "Employee #", 
+     CONCAT(first_name, " ", last_name) AS "Full Name"
 FROM employees;
 
--- How many records do we have in employees? --
+-- Query 2: How many males and female employees do we have?  ---
 SELECT 
-	COUNT(1) as "Total Employees"
-FROM employees;
-
--- How many males and female employees do we have  
-SELECT 
-	gender, COUNT(gender) as "Frequency"
+	gender, COUNT(gender) AS "Frequency"
 FROM employees
 WHERE gender = "M" OR gender = "F"
 GROUP BY gender 
 ORDER BY gender DESC;
 
--- How many Distinct jobs do we have? --
-SELECT DISTINCT dept_name
-FROM departments;
+-- Query 5: Do men and women get paid equally? (Spoiler alert, they do :D ) --
+SELECT 
+	employees.gender, 
+    AVG(salaries.salary) as "Average Salary $"
+FROM employees
+INNER JOIN salaries ON employees.emp_no=salaries.emp_no
+GROUP BY gender;
+
+
+
+
+
+
+-- IGNORE BELOW. Validating query 5 by calculating individually--
+/*
+SELECT employees.gender, AVG(salaries.salary)
+FROM employees
+INNER JOIN salaries ON employees.emp_no=salaries.emp_no
+WHERE employees.gender = "F";
+
+SELECT employees.gender, AVG(salaries.salary)
+FROM employees
+INNER JOIN salaries ON employees.emp_no=salaries.emp_no
+WHERE employees.gender = "M";
+*/
 
